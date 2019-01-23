c9b03779e152fb3895956e869b4bd87daf4a0ae5
YuOlvera <46759287+YuOlvera@users.noreply.github.com>
2019-01-23 07:53:59 -0600

    Update main.java

--- main.java
+++ main.java
@@ -2,14 +2,35 @@
 #include<math.h>
 int main (){
 	int n; //Para las n dimensiones
-	int t;
 	int bytes;
+	int inf; //Límite inferior
+	int sup; //Límite superior
+	
+	
+	
 	printf("\nCalculador de Dirección en un Punto dentro de un Arreglo N-Dimensional\n");
 	printf();
 	printf("Tamaño del arreglo (N dimensiones): ");
 	scanf("%d",&n);
 	printf("Tamaño del tipo de dato(entero,caracter,real,etc) en bytes: ");
 	scanf("%d",&bytes);
+	
+	//DECLARACIÓN DE LOS ARREGLOS
+	int inferiores[n];
+	
+	//SE LE PIDE AL USUARIO LOS LIMITES INFERIORES Y SUPERIORES
+	for(int j=1; j<=n; j++){
+		printf("Límite inferior %d: \n",j);
+		scanf("%d",&lim); 
+		
+		
+	
+	}
+	
+	printf("Ingrese las posiciones a calcular\n");
+	for(int i=1; i<=4; i++){
+		
+	}
 	
 return 0;
 }
