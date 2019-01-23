5224433f8d3668ce14955c6bb885b989e860ab30
YuOlvera <46759287+YuOlvera@users.noreply.github.com>
2019-01-23 07:44:01 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<math.h>
 int main (){
-	
+	int n; //Para las n dimensiones
+	int bytes;
 	printf("\nCalculador de Dirección en un Punto dentro de un Arreglo N-Dimensional\n");
 	printf();
+	printf("Tamaño del arreglo (N dimensiones): ");
+	scanf("%d",&n);
+	printf("Tamaño del tipo de dato(entero,caracter,real,etc) en bytes: ");
+	scanf("%d",&bytes);
 	
 return 0;
 }
